a09904aa75076b4500e4102b377702b4000c64ae
scastd <scastd00@gmail.com>
2024-08-05 22:55:01 +0200

    Add Critical class implementation
    Introduce Critical class for managing critical sections in parallel tasks using synchronized blocks with unique locks per section. Add unit tests in CriticalTests to verify correct behavior of critical sections in various scenarios.

---
+++ src/test/java/jromp/parallel/construct/critical/CriticalTests.java
@@ -0,0 +1,95 @@
+package jromp.parallel.construct.critical;
+
+import jromp.parallel.Parallel;
+import jromp.parallel.var.SharedVariable;
+import jromp.parallel.var.Variable;
+import jromp.parallel.var.Variables;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CriticalTests {
+    @Test
+    void testSimple() {
+        Variables variables = Variables.create().add("x", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables,
+                       (id, vars) -> Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1)))
+                .join();
+
+        assertThat(variables.get("x").value()).isEqualTo(4);
+    }
+
+    @Test
+    void testSameNameMulti() {
+        Variables variables = Variables.create().add("x", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables,
+                       (id, vars) -> {
+                           Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1));
+                           Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1));
+                       })
+                .join();
+
+        assertThat(variables.get("x").value()).isEqualTo(8);
+    }
+
+    @Test
+    void testDifferentName() {
+        Variables variables = Variables.create()
+                                       .add("x", new SharedVariable<>(0))
+                                       .add("y", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables,
+                       (id, vars) -> {
+                           Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1));
+                           Critical.enter("y", id, vars, (i, v) -> v.<Integer>get("y").update(y -> y + 1));
+                       })
+                .join();
+
+        assertThat(variables.get("x").value()).isEqualTo(4);
+        assertThat(variables.get("y").value()).isEqualTo(4);
+    }
+
+    @Test
+    void testDifferentNameMulti() {
+        Variables variables = Variables.create()
+                                       .add("x", new SharedVariable<>(0))
+                                       .add("y", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables,
+                       (id, vars) -> {
+                           Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1));
+                           Critical.enter("y", id, vars, (i, v) -> v.<Integer>get("y").update(y -> y + 1));
+                           Critical.enter("x", id, vars, (i, v) -> v.<Integer>get("x").update(x -> x + 1));
+                           Critical.enter("y", id, vars, (i, v) -> v.<Integer>get("y").update(y -> y + 1));
+                       })
+                .join();
+
+        assertThat(variables.get("x").value()).isEqualTo(8);
+        assertThat(variables.get("y").value()).isEqualTo(8);
+    }
+
+    @Test
+    void testMoreOperationsInsideCritical() {
+        Variables variables = Variables.create().add("x", new SharedVariable<>(new StringBuilder()));
+
+        Parallel.withThreads(4)
+                .block(variables,
+                       (id, vars) -> {
+                           Critical.enter("x", id, vars, (i, v) -> {
+                               Variable<StringBuilder> variable = v.get("x");
+                               variable.update(sb -> sb.append("x"));
+                               variable.update(sb -> sb.append(" "));
+                               variable.update(sb -> sb.append("more"));
+                           });
+                       })
+                .join();
+
+        assertThat(variables.get("x").value().toString()).hasToString("x more".repeat(4));
+    }
+}
