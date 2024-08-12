084283c7e1f875777691fcb4fbf5eb5fe61b9375
scastd <scastd00@gmail.com>
2024-08-12 11:31:19 +0200

    Add read, write, and update atomic tests
    Renamed `testSimpleAtomicConstruct` to `testUpdate` and added comprehensive tests for atomic `read`, `write`, and `update` operations. These tests ensure correct parallel handling of atomic variables and validate their states before and after parallel execution.

--- src/test/java/jromp/parallel/construct/atomic/AtomicTests.java
+++ src/test/java/jromp/parallel/construct/atomic/AtomicTests.java
@@ -10,7 +10,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AtomicTests {
     @Test
-    void testSimpleAtomicConstruct() {
+    void testRead() {
+        Variables variables = Variables.create().add("x", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables, (id, vars) -> {
+                    Integer value = Atomic.read("x", vars);
+
+                    assertThat(((SharedVariable<Integer>) variables.<Integer>get("x")).hasAtomic()).isTrue();
+                    assertThat(value).isZero();
+                })
+                .join();
+
+        // The atomic variable is removed after the parallel block is executed
+        assertThat(((SharedVariable<Integer>) variables.<Integer>get("x")).hasAtomic()).isFalse();
+    }
+
+    @Test
+    void testWrite() {
+        Variables variables = Variables.create().add("x", new SharedVariable<>(0));
+
+        Parallel.withThreads(4)
+                .block(variables, (id, vars) -> {
+                    Atomic.write("x", 1, vars);
+
+                    // Only the atomic variable should be updated here
+                    assertThat(((SharedVariable<Integer>) variables.<Integer>get("x")).hasAtomic()).isTrue();
+                    assertThat(variables.get("x").value()).isEqualTo(0);
+                })
+                .join();
+
+        // The atomic variable changes its value to the final value on the "parent" shared variable
+        // after the parallel block is executed, and the atomic variable is removed.
+        assertThat(variables.get("x").value()).isEqualTo(1);
+        assertThat(((SharedVariable<Integer>) variables.<Integer>get("x")).hasAtomic()).isFalse();
+    }
+
+    @Test
+    void testUpdate() {
         Variables variables = Variables.create().add("x", new SharedVariable<>(0));
 
         Parallel.withThreads(4)
