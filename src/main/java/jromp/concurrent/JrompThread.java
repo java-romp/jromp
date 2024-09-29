f154b72696af321f93d87a2ff4c9c8f803e1ada1
scastd <scastd00@gmail.com>
2024-09-29 03:26:07 +0200

    Added javadoc to `JrompThread` class

--- src/main/java/jromp/concurrent/JrompThread.java
+++ src/main/java/jromp/concurrent/JrompThread.java
@@ -2,11 +2,34 @@ package jromp.concurrent;
 
 import java.util.concurrent.ThreadFactory;
 
+/**
+ * A thread class that extends {@link Thread} and provides additional information about the thread.
+ */
 public class JrompThread extends Thread {
+    /**
+     * The thread ID.
+     */
     private final int tid;
+
+    /**
+     * The team ID.
+     */
     private final int team;
+
+    /**
+     * The thread name.
+     */
     private final String threadName;
 
+    /**
+     * Constructs a new {@link JrompThread} with the given {@link Runnable}, thread ID, and team ID.
+     *
+     * @param runnable the {@link Runnable} to be executed by this thread.
+     * @param tid      the thread ID.
+     * @param team     the team ID.
+     *
+     * @see #generateThreadName(int, int)
+     */
     public JrompThread(Runnable runnable, int tid, int team) {
         super(runnable);
 
@@ -29,6 +52,14 @@ public class JrompThread extends Thread {
         return threadName;
     }
 
+    /**
+     * Generates a thread name with the given team ID and thread ID.
+     *
+     * @param team the team ID.
+     * @param tid  the thread ID.
+     *
+     * @return the generated thread name.
+     */
     private static String generateThreadName(int team, int tid) {
         return "%s-%d-%d".formatted(JrompThread.class.getSimpleName(), team, tid);
     }
@@ -42,11 +73,31 @@ public class JrompThread extends Thread {
         return new JrompThreadFactory(threadsPerTeam);
     }
 
+    /**
+     * A {@link ThreadFactory} that creates new {@link JrompThread} instances.
+     * The number of threads per team is specified in the constructor.
+     */
     private static class JrompThreadFactory implements ThreadFactory {
+        /**
+         * The number of threads per team.
+         */
         private final int threadsPerTeam;
+
+        /**
+         * The thread ID.
+         */
         private int tid = 0;
+
+        /**
+         * The team ID.
+         */
         private int team = 0;
 
+        /**
+         * Constructs a new {@link JrompThreadFactory} with the given number of threads per team.
+         *
+         * @param threadsPerTeam the number of threads per team.
+         */
         public JrompThreadFactory(int threadsPerTeam) {
             this.threadsPerTeam = threadsPerTeam;
         }
