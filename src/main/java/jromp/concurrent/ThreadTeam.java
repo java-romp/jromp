0726df8eee70264a230d710dce5a1ba73623fbad
scastd <scastd00@gmail.com>
2024-10-07 03:01:52 +0200

    Added javadoc to ThreadTeam

--- src/main/java/jromp/concurrent/ThreadTeam.java
+++ src/main/java/jromp/concurrent/ThreadTeam.java
@@ -2,15 +2,36 @@ package jromp.concurrent;
 
 import java.util.ArrayList;
 
+/**
+ * A thread team is a group of threads that are working together
+ * to execute some parallel task.
+ */
 public class ThreadTeam {
+    /**
+     * The list of threads in this team.
+     */
     private final ArrayList<JrompThread> threads;
+
+    /**
+     * The team ID.
+     */
     private final int teamId;
 
+    /**
+     * Constructs a new {@link ThreadTeam} with the given team ID.
+     *
+     * @param teamId the team ID.
+     */
     ThreadTeam(int teamId) {
         this.teamId = teamId;
         this.threads = new ArrayList<>();
     }
 
+    /**
+     * Adds the given {@link JrompThread} to this team.
+     *
+     * @param thread the {@link JrompThread} to be added.
+     */
     void addThread(JrompThread thread) {
         threads.add(thread);
     }
