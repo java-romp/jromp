150ec6b9aab29581f8ff1c805a236a590a137961
GitHub Action <action@github.com>
2024-10-18 15:46:13 +0000

    chore: updates index.html files incl. javadoc versions

---
+++ javadoc/2.2.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jromp"},{"l":"jromp.concurrent"},{"l":"jromp.construct.atomic"},{"l":"jromp.construct.critical"},{"l":"jromp.operation"},{"l":"jromp.task"},{"l":"jromp.var"},{"l":"jromp.var.reduction"}];updateSearchResults();
